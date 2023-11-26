a912786747a8819ec5d3c29434f47ac86d8583a9
vidaang <94209946+vidaang@users.noreply.github.com>
2023-11-26 18:47:12 -0500

    Finished Menu, Navbar, & Footer

--- cap4102/src/components/navbar.jsx
+++ cap4102/src/components/navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import logo from "../images/header_logo_1.png";
+import '../index.css';
 
 export const Navbar = () => {
   return (
-    <div className='nav'>navbar
-        <div>
-            <Link to="/">Home </Link>
-            <Link to="/menu">Menu</Link>
+    <div className="navbar">
+        <div className="navbar-container">
+            <img src={logo} className="img-header-logo"></img>
+            <div className="navbar-links">
+                <Link to="/">HOME</Link>
+                <Link to="/about">ABOUT</Link>
+                <Link to="/menu">MENU</Link>
+                <Link to="/reviews">REVIEWS</Link>
+                <Link to="/directions">DIRECTIONS</Link>
+                <Link to="/contact">CONTACT US</Link>
+            </div>
         </div>
     </div>
   )
--- cap4102/src/pages/menu/menu.jsx
+++ cap4102/src/pages/menu/menu.jsx
@@ -1,12 +1,72 @@
-import { ITEMS } from "../../menu_items";
+import { ITEMS } from "../../components/menu_items";
 import { Item } from "./menu_item";
+import '../../index.css';
 
 export default function Menu() {
+  const a = "Appetizers";
+  const e = "Entrees";
+
+  const appetizers = ITEMS.filter(item => item.category === a);
+  const entrees = ITEMS.filter(item => item.category === e);
+
   return (
-    <div className="Menu">
-        <p>help me IM THE MENU</p>
-        {ITEMS.map((menu_item) => 
-        (<Item data={menu_item}/>))}
+    <div className="page-container">
+      <div className="menu-container">
+        <header className="menu-header">
+          <h1 className="menu-header-text">Menu</h1>
+        </header>
+
+        <body className="menu-body">
+          <p className="menu-body-title">
+            Discover a blend of flavors from Da Nang and Phan Thiết in our curated menu!
+            Made with love and served with a side of culture, each dish is a taste of our rich Vietnamese heritage.
+          </p>
+
+          <h1>Appetizers</h1>
+          <div className="menu-items-group">
+            {appetizers.map((menu_item) => (
+              <Item key={menu_item.id} data={menu_item} />
+            ))}
+          </div>
+
+          <h1>Entrees</h1>
+          <div className="menu-items-group">
+            {entrees.map((menu_item) => (
+              <Item key={menu_item.id} data={menu_item} />
+            ))}
+          </div>
+
+          <h1>Beverages</h1>
+          <div className="menu-items-group">
+            <p className="menu-body-text">
+              Water
+            </p>
+            <p className="menu-body-text">
+              Soda (Coke, Sprite, Dr. Pepper)
+            </p>
+            <p className="menu-body-text">
+              Coconut Water
+            </p>
+            <p className="menu-body-text">
+              Vietnamese Coffeee
+            </p>
+          </div>
+
+          <h1>Upgrades</h1>
+          <div className="menu-box">
+            <div className="menu-items-group">
+              <p className="menu-body-text">
+                Substitute White Rice w/Fried Rice
+              </p>
+              <p className="menu-body-text">
+                Substitute White Rice w/Stir Fry Noodles
+              </p>
+            </div>
+          </div>
+
+        </body>
+
+      </div>
     </div>
   );
-};
+}
