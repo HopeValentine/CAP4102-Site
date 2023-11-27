6caeb4cf8258ef148dcb3711170c5fd6c9386a9e
vidaang <94209946+vidaang@users.noreply.github.com>
2023-11-26 19:01:27 -0500

    Styling fix

--- cap4102/src/components/navbar.jsx
+++ cap4102/src/components/navbar.jsx
@@ -7,9 +7,10 @@ export const Navbar = () => {
   return (
     <div className="navbar">
         <div className="navbar-container">
-            <img src={logo} className="img-header-logo"></img>
+            <Link to="/">
+                <img src={logo} alt="Logo" className="img-header-logo" />
+            </Link>
             <div className="navbar-links">
-                <Link to="/">HOME</Link>
                 <Link to="/about">ABOUT</Link>
                 <Link to="/menu">MENU</Link>
                 <Link to="/reviews">REVIEWS</Link>
